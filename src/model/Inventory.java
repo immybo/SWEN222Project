ce47721379be84508a3f319acc702213731ec9d4
Robert Campbell <robertcampbellnz@gmail.com>
2016-09-21 11:43:18 +1200

    add item abstract methods (and inventory empty class)

---
+++ src/model/Inventory.java
@@ -0,0 +1,12 @@
+package model;
+
+/**
+ * Defines something which can contain a certain
+ * amount of items. Items can usually be taken out
+ * of or put into an inventory.
+ * 
+ * @author Robert Campbell
+ */
+public abstract class Inventory {
+	// TODO
+}
