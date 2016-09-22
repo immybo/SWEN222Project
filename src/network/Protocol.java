572ecb022f6b5aa23ceaf8c5fc561824ece2434e
David Phillips <david@sighup.nz>
2016-09-22 22:03:56 +1200

    Add a sanity check when clients connect

---
+++ src/network/Protocol.java
@@ -0,0 +1,6 @@
+package network;
+
+public class Protocol {
+	public static final String serverMagic = "Yes helo does the mcborger contain vegetal\n";
+	public static final String clientMagic = "no\n";
+}
