8d57f7b684eb478a22e0218cbab92238c3468591
Robert Campbell <robertcampbellnz@gmail.com>
2016-09-22 17:53:20 +1200

    add buttons in informationpanel

--- src/view/InformationPanel.java
+++ src/view/InformationPanel.java
@@ -1,7 +1,11 @@
 package view;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 /**
  * The panel which displays information that can't be
@@ -10,9 +14,58 @@ import java.awt.*;
  * @author Robert Campbell
  */
 public class InformationPanel extends JPanel {
+	private JButton exitButton;
+	private JButton saveButton;
+	private JButton loadButton;
+	
+	public InformationPanel(){
+		this.setLayout(new BorderLayout());
+		
+		exitButton = new JButton("Exit");
+		exitButton.addActionListener((ActionEvent e)->{
+			int result = JOptionPane.showConfirmDialog(this.getParent(), "Are you sure you want to exit?",
+					"", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+			if(result == 0)
+				exitGame();
+		});
+		
+		saveButton = new JButton("Save Game");
+		saveButton.addActionListener((ActionEvent e)->{
+			saveGame();
+		});
+		
+		loadButton = new JButton("Load Game");
+		loadButton.addActionListener((ActionEvent e)->{
+			int result = JOptionPane.showConfirmDialog(this.getParent(), "Are you sure you want to exit this game and load another?",
+					"", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+			if(result == 0)
+				loadGame();
+		});
+		
+		JPanel buttonPanel = new JPanel();
+		buttonPanel.setLayout(new GridLayout(2, 2));
+		
+		buttonPanel.add(saveButton, 0);
+		buttonPanel.add(loadButton, 1);
+		buttonPanel.add(exitButton, 2);
+		
+		this.add(buttonPanel, BorderLayout.SOUTH);
+	}
+	
     @Override
     public void paint(Graphics g){
-        g.setColor(Color.RED);
-        g.fillRect(0,0,this.getWidth(),this.getHeight());
+        super.paint(g);
+    }
+    
+    private void exitGame(){
+    	System.exit(0);
+    }
+    
+    private void loadGame(){
+    	System.out.println("Load game button pressed");
+    }
+    
+    private void saveGame(){
+    	System.out.println("Save game button pressed");
     }
 }
