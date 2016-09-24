090c17a49971ade47c4bea5536435c3b97316b7a
Robert Campbell <robertcampbellnz@gmail.com>
2016-09-24 20:06:24 +1200

    add direction util class

---
+++ src/util/Direction.java
@@ -0,0 +1,33 @@
+package util;
+
+import java.awt.Point;
+
+/**
+ * Defines one of the four cardinal directions that can be moved in.
+ * 
+ * @author Robert Campbell
+ */
+public class Direction {
+	public static final int NORTH = 0;
+	public static final int EAST = 1;
+	public static final int SOUTH = 2;
+	public static final int WEST = 3;
+	
+	public static PointD move(PointD initial, int direction, double amount){
+		if(direction == NORTH)
+			return initial.add(0, -amount);
+		else if(direction == EAST)
+			return initial.add(amount, 0);
+		else if(direction == SOUTH)
+			return initial.add(0, amount);
+		else if(direction == WEST)
+			return initial.add(-amount, 0);
+		
+		throw new IllegalArgumentException(direction + " is not a valid direction!");
+	}
+	
+	public static Point move (Point initial, int direction, int amount){
+		PointD moved = move(new PointD(initial.x, initial.y), direction, amount);
+		return new Point((int)moved.X, (int)moved.Y);
+	}
+}
