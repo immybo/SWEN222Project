78c7ca4b9a7030e951cef8e977a4dc8c619b9e3b
David Phillips <david@sighup.nz>
2016-09-27 11:45:24 +1300

    Add non-blocking send and rec to ServerThread

--- src/network/ServerThread.java
+++ src/network/ServerThread.java
@@ -18,6 +18,10 @@ public class ServerThread extends Thread {
 	/* back reference to our parent/controlling Server object */
 	private Server parentServer;
 	
+	/* flag to indicate whether or not there is game state which requires
+	 * sending to the client */
+	private boolean postFlag = false;
+	
 	/* socket connected to client and its streams */
 	private Socket socket;
 	private DataInputStream in;
@@ -29,11 +33,51 @@ public class ServerThread extends Thread {
 		this.parentServer = server;
 	}
 	
+	/**
+	 * Post a notification of a change made to the game state
+	 * that needs sending to the client
+	 */
+	synchronized public void post() {
+		postFlag = true;
+	}
+	
+	/**
+	 * If required by a previous uncleared post, send the game
+	 * state to the client and clear the post request
+	 */
+	synchronized private void processDownstream() {
+		if (!postFlag)
+			return;
+		
+		/* FIXME send the updated game state to the client */
+	
+		postFlag = false;
+	}
+	
+	/**
+	 * Process any data being sent to us from the client
+	 */
+	private void processUpstream() {
+		/* FIXME do something here */
+	}
+	
 	@Override
 	public void run() {
 		try {
 			in = new DataInputStream(socket.getInputStream());
 			out = new DataOutputStream(socket.getOutputStream());
+			
+			while(true) {
+				/* muh non-blocking-ness */
+				/* FIXME check if processing only one upstream packet before processing downstream traffic hurts performace.
+				 * Might be better to, say, process between 0 and 5 upstream packets if they are there
+				 * before going on to process downstream. Dunno.
+				 */
+				if (in.available() > 0) {
+					processUpstream();
+				}
+				processDownstream();
+			}
 		} catch (IOException e) {
 			System.err.println("Error occurred; stopping the server:"+e.getMessage());
 			parentServer.stop();
