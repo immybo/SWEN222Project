5feb8e8d1083af9216d7fc997fcce2c6c8f39a1d
crusadre <martin.chau123@hotmail.com>
2016-10-05 12:37:37 +1300

    Useing a key

--- src/model/UseKey.java
+++ src/model/UseKey.java
@@ -18,6 +18,32 @@ package model;
 
 		@Override
 		public void execute(Player p) {
+			// get player inventory
+			Item[] pInven = p.getInventory().getItems();
+			Key matchKey = null;
+			// for every item check if the gate can be opened by it
+			for(Item i: pInven){
+				if(i instanceof Key){
+					Key k = (Key) i;
+					if(keyGate.openedBy(k)){
+						matchKey = k;
+					}
+				}
+			}
+			if(matchKey == null) {
+				//show dialog of "Your keys dont seem to fit"
+			} else {
+				//set the gate to open
+				keyGate.unlock();
+				keyGate.open();
+				keyGate.setDrawID(matchKey.getKeyID() + "-open");
+				//remove key from play inventory
+				p.getInventory().removeItem(matchKey);
+				//remove interaction from gate
+				keyGate.removeInteraction(this);
+				keyGate.addInteraction(new Inspect("It looks to be an open gate."));
+			}
+			
 		}
 		
 
