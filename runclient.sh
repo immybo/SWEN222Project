77d72cad37d05e7a84674fb9daaea4e5c731fe4d
David Phillips <david@sighup.nz>
2016-10-06 11:48:34 +1300

    Add some simple convenience scripts for running from command line

---
+++ runclient.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Convenience script for running client/game
+
+java -cp target/classes Main
---
+++ runserver.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+# Convenience script for running server
+
+java -cp target/classes network.Server
