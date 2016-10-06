e9172b0ddc07eae7c31541c4151c903e938d3968
David Phillips <david@sighup.nz>
2016-10-06 23:10:26 +1300

    Add another test case to DirectionTests

--- src/tests/DirectionTests.java
+++ src/tests/DirectionTests.java
@@ -7,13 +7,31 @@ import util.Direction;
 
 public class DirectionTests extends TestCase {
 
+	private static final Direction n = new Direction(Direction.NORTH);
+	private static final Direction s = new Direction(Direction.SOUTH);
+	private static final Direction e = new Direction(Direction.EAST);
+	private static final Direction w = new Direction(Direction.WEST);
+	
+	@Test
+	public void testEquality() {
+		/* check for equality */
+		assertEquals(n,n);
+		assertEquals(e,e);
+		assertEquals(s,s);
+		assertEquals(w,w);
+		assertEquals(n, new Direction(Direction.NORTH));
+		assertEquals(e, new Direction(Direction.EAST));
+		assertEquals(s, new Direction(Direction.SOUTH));
+		assertEquals(w, new Direction(Direction.WEST));
+		
+		/* inequality checks */
+		assertFalse(n.equals(null));
+		assertFalse(n.equals("Top kek m'lady"));
+		assertFalse(n.equals(e));
+	}
+	
 	@Test
 	public void testOpposite() {
-		Direction n = new Direction(Direction.NORTH);
-		Direction s = new Direction(Direction.SOUTH);
-		Direction e = new Direction(Direction.EAST);
-		Direction w = new Direction(Direction.WEST);
-		
 		assertEquals(Direction.oppositeDirection(n), s);
 		assertEquals(Direction.oppositeDirection(e), w);
 		assertEquals(Direction.oppositeDirection(s), n);
