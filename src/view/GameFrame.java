a33e243f9c51e187382ebaeeb45293893da91a9f
R. Campbell <robertcampbellnz@gmail.com>
2016-10-08 14:54:57 +1300

    clean up game frame

--- src/view/GameFrame.java
+++ src/view/GameFrame.java
@@ -12,16 +12,21 @@ import java.lang.Thread.UncaughtExceptionHandler;
 
 /**
  * The main frame, which contains all of the components for
- * the actual game.
+ * the actual game. This frame is a singleton; GameFrame.instance()
+ * should be called to get the instance.
  *
  * @author Robert Campbell
  */
 public class GameFrame extends JFrame {
+	// The sole instance of GameFrame in the program
 	private static GameFrame frame;
 	
-    private static final int DEFAULT_WIDTH = 3000;
-    private static final int DEFAULT_HEIGHT = 3000;
+	// If the screen is big enough, the frame width and height
+	// will initially be set to these values
+    private static final int DEFAULT_WIDTH = 1280;
+    private static final int DEFAULT_HEIGHT = 768;
 
+    // As a proportion of the frame's total width
     private static final double INFORMATION_PANEL_WIDTH = 0.3;
 
     // Shows the actual game map, entities, etc.
@@ -30,6 +35,9 @@ public class GameFrame extends JFrame {
     // For example, their inventory, health
     private InformationPanel informationPanel;
 
+    /**
+     * Internal constructor for GameFrame.
+     */
     private GameFrame(){
         setSizeDefault();
         this.setLayout(new BorderLayout());
@@ -66,20 +74,16 @@ public class GameFrame extends JFrame {
     
     /**
      * Sets the size of this frame to be a reasonable default.
+     * This size will never be bigger than the screen size.
      */
     private void setSizeDefault(){
     	Dimension screenSize = getScreenSize();
     	int width = DEFAULT_WIDTH;
-    	if(width > screenSize.getWidth() * 0.9)
-    		width = (int)(screenSize.getWidth() * 0.9);
+    	if(width > screenSize.getWidth() * 0.7)
+    		width = (int)(screenSize.getWidth() * 0.7);
     	int height = DEFAULT_HEIGHT;
-    	if(height > screenSize.getHeight() * 0.9)
-    		height = (int)(screenSize.getHeight() * 0.9);
-    	
-    	if(width > 1024){
-    		width = 1024;
-    		height = (int)(1024 * ((double)screenSize.getHeight() / screenSize.getWidth()));
-    	}
+    	if(height > screenSize.getHeight() * 0.7)
+    		height = (int)(screenSize.getHeight() * 0.7);
     	
     	this.setPreferredSize(new Dimension(width, height));
     }
@@ -97,16 +101,35 @@ public class GameFrame extends JFrame {
     	return canvas;
     }
     
+    /**
+     * Displays a dialog box with boolean options
+     * to the user.
+     * 
+     * @param title The title of the dialog box.
+     * @param text The text to be displayed in the dialog box.
+     * @return 0 if yes was selected; 1 if no was selected or the window was closed.
+     */
     public int displayBooleanQuestion(String title, String text){
     	String[] options = { "Yes" , "No" };
     	return JOptionPane.showOptionDialog(this, text, title, JOptionPane.DEFAULT_OPTION,
     										JOptionPane.QUESTION_MESSAGE, null, options, 1);
     }
     
+    /**
+     * Displays a message box to the user.
+     * 
+     * @param text The text to be displayed in the dialog box.
+     */
     public void displayMessage(String text){
     	JOptionPane.showMessageDialog(this, text);
     }
 	
+    /**
+     * Attempts to connect this client to a given
+     * hostname.
+     * @param hostname
+     * @throws NetworkError
+     */
 	public void connect(String hostname) throws NetworkError {
 		Client client = new Client(this, hostname);
 		informationPanel.setClient(client);
@@ -120,11 +143,28 @@ public class GameFrame extends JFrame {
 		client.run();
 	}
 	
+	/**
+	 * Displays a dialog explaining a network error, 
+	 * the cause of which is the given throwable.
+	 * 
+	 * @param e The cause of the network error.
+	 */
 	public void showNetworkErrorBox(Throwable e) {
 			JOptionPane.showMessageDialog(this.getParent(), "Network Error: "+e.getMessage(),
 				"Error", JOptionPane.OK_OPTION);
 	}
     
+	/**
+	 * Gets the current instance of GameFrame,
+	 * or creates a new one if no instance is
+	 * present (i.e. on the first call to this
+	 * method in the program.
+	 * 
+	 * A GameFrame should only be gotten through
+	 * this method.
+	 * 
+	 * @return The instance of GameFrame.
+	 */
     public static GameFrame instance(){
     	if(frame != null){
     		return frame;
