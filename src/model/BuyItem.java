626d9abf0d07a4877df82d2b52646c42d589a26b
Karametua <karametua@gmail.com>
2016-10-11 17:13:57 +1300

    Deleted print statements

--- src/model/BuyItem.java
+++ src/model/BuyItem.java
@@ -38,6 +38,13 @@ public class BuyItem extends Interaction implements Storable, Serializable {
 		this.cost = cost;
 	}
 	
+	/**
+	 * Alternative constructor for loading, Entity then is set to the relevant entity
+	 * 
+	 * @param item Item that is bought
+	 * @param itemName Name of item to be bought
+	 * @param cost
+	 */
 	public BuyItem(Item item, String itemName, int cost) {
 		this.item = item;
 		this.itemName = itemName;
